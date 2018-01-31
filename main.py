8e3d4959e5fea120cbb12f322c1161bb33748c07
Fabricio Robles <fabricio@fabro.fabro.com>
2018-01-30 21:06:19 -0600

    Proyecto sin terminar de pasar input capturado por POST

--- main.py
+++ main.py
@@ -1,80 +1,81 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import webapp2
 
 form="""
-<form method="post" action="/testform"> 
-	<input name="q"> 
-	<input type="submit">
-</form> 
-"""
-
-form2="""
-<form>
-	<label>
-	One
-	<input id="1" type="radio" name="q" value="one"> 
-	</label>
-	
-	<label>
-	Two
-	<input id="2" type="radio" name="q" value="two"> 
-	</label>
-	
-	<label>
-	Three
-	<input id="3" type="radio" name="q" value="three"> 
-	</label>
-	
-	
+<form method="post"> 
+	Whats is your birthday?
 	<br>
 	
-	<input type="submit">
-</form> 
-"""
-
-form3="""
-<form>
-	<select name = q>
-		<option value="1">one</option>
-		<option>two</option>
-		<option>three</option>
-	</select>
+	<label> Month
+		<input type="text" name"month">
+	</label>
 	
+	<label> Day
+		<input type="text" name"day">
+	</label>
 	
-	<br>
+	<label> Year
+		<input type="text" name"year"> 
+	</label>
 	
 	<input type="submit">
 </form> 
 """
 
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+          
+month_abbvs = dict((m[:3].lower(),m) for m in months)
+
+
+			
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(form3)
+        self.response.out.write(form)
 		
-class TestHandler(webapp2.RequestHandler):		
+    def post(self):
+	user_month = valid_month(self.request.get("month"))
+	user_day = valid_day(self.request.get("day"))
+	user_year = valid_year(self.request.get("year"))
+	
+		
+	if not (user_month and user_day and user_year):
 
-	def post(self):
-		#q = self.request.get("q")
-		#self.response.write(self.response)
-		
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write(self.request) 
-		
+		self.response.out.write(user_year)
+		self.response.out.write(user_day)
+		self.response.out.write(user_month)
+	
+	else:
+		self.response.out.write("Information ok!!!!!")
+
 app = webapp2.WSGIApplication([
-    ('/', MainPage),('/testform',TestHandler)
+    ('/', MainPage),
 ], debug=True)
+
+
+def valid_month(month):
+    if month:
+        short_month = month[:3].lower()
+        return month_abbvs.get(short_month)
+		
+def valid_day(day):
+    if day.isdigit():
+         daynumber = int(day)
+         if daynumber >= 1 and daynumber <= 31:
+                return daynumber
+				
+def valid_year(year):
+    if year and year.isdigit():
+        year = int(year)
+        if year >= 1900 and year <= 2020:
+            return year
